518861217335bf3a4a57103d0d73205137c83cd4
Nancy Golden <pngolden@verizon.net>
2020-06-17 21:17:25 -0500

    Making progress and having a lot of fungit status

--- oop3-abstract-morph.js
+++ oop3-abstract-morph.js
@@ -1,6 +1,10 @@
 
 /*
-1. Create a Abstract Creature class that is NOT allowed to be instantiated. Find a way to prevent this class from being instantiated with an instance.  In the Creature class, include two abstract methods.  move(), and act().  These methods are NOT allowed to be invoked from the Abstract class, so throw an error in these methods if they are invoked by any inherited sub-classes.  
+1. Create a Abstract Creature class that is NOT allowed to be instantiated.
+//Find a way to prevent this class from being instantiated with an instance.  
+//In the Creature class, include two abstract methods.  move(), and act().  
+//These methods are NOT allowed to be invoked from the Abstract class, so 
+throw an error in these methods if they are invoked by any inherited sub-classes.  
 
 Create at least 3 sub-classes that inherit from the Abstract Creature Class.  Example sub-classes are: 
     -Human
@@ -9,7 +13,9 @@ Create at least 3 sub-classes that inherit from the Abstract Creature Class.  Ex
     -Reptile
     -Fish
 
-These sub-classes MUST implement the abstract methods of move() and act(), which are found on the abstract Creature class. In addition to the 2 abstract methods, implement 1 unique method on each sub-class.  Also, each sub-class MUST have at least 2 unique property fields on them.  Example property fields are:
+These sub-classes MUST implement the abstract methods of move() and act(), which are found on the 
+abstract Creature class. In addition to the 2 abstract methods, implement 1 unique method on each sub-class.
+Also, each sub-class MUST have at least 2 unique property fields on them.  Example property fields are:
     -name
     -weight
     -food
@@ -17,19 +23,136 @@ These sub-classes MUST implement the abstract methods of move() and act(), which
 
 */
 
-    class Creature {
-        //your code here...
+    class Creature { //Abstract
+
+        constructor() {
+            if(this.constructor == Creature) {
+                throw new Error("Unable to instantiate Abstract Class Creature");
+            }
+        }
+        
+        move(){
+            //console.log ("Move is a good thing to do - don't forget to get up and stretch");
+            //Don't want to do above line because you don't invoke functions outside
+            //of abstract method even though inheritance allows you to.
+            //Instead - do throw new Error
+             
+            throw new Error("This is an abstract method - Don't even think about invoking it!");
+        };
+    
+        act(){
+          
+            throw new Error("This is an abstract method - Don't make me have to call the code police!");
+        };
+
     }
 
-    class Human extends Creature {
-        //your code here...
+    //const creature = new Creature(); //Code to test and introduce error
+
+    //console.log(creature); //Errors out which shows cannot be instantiated
+
+    //Conclusion: Creature is an abstract class so I can't instantiate it
+    //but I can still inherit from it. To prevent that you have to throw new Error.
+    
+    class Human extends Creature { //Used Human Class to illustrate 
+        //can't invoke methods from Abstract Parent Creatures
+
     }
 
+    //const mortal = new Human();
 
+    //mortal.move(); introduces error to show methods can't be invoked from Abstract Creature Class
+    //mortal.act();
+//WE ARE IMPLEMENTING THE CONCEPT OF MOVE AND ACT IN THE ABSTRACT
+//IN THE CHILD
+    class Bird extends Creature{ //Uses keyword extends to inherit from Creature class
 
+        
+
+        //Bird is child class and Creature is the parent class; Bird inherits
+        //all of the functions/methods/properties from parent but it can't invoke methods because 
+        //the Creature Class is an Abstraction so create the methods in the subclass itself:
+        
+        move(){
+            console.log ("Birds move by flying");
+    
+        }
+        act(){
+            console.log ("Birds act aggressively when protecting their young");
+    
+        }
+        sing(){
+            console.log ("Birds often sing in the early morning hours");
+    
+        }
+    
+    } 
+    
+    const avian = new Bird();
+
+    avian.move();
+    avian.act();
+    avian.sing();
+
+    class Fish extends Creature{ //Uses keyword extends to inherit from Creature class
+
+        //Fish is child class and Creature is the parent class; Fish inherits
+        //all of the functions/methods/properties from parent but it can't invoke methods because 
+        //the Creature Class is an Abstraction so create the methods in the subclass itself:
+        
+        move(){
+            console.log ("Fish move by swimming");
+    
+        }
+        act(){
+            console.log ("Fish act very excited at feeding time");
+    
+        }
+        hide(){
+            console.log ("Fish often hide when bigger fish are near");
+    
+        }
+    
+    } 
+    
+    const watercreature = new Fish();
+
+    watercreature.move();
+    watercreature.act();
+    watercreature.hide();
+
+    class Dinosaur extends Creature{ 
+
+        
+        //constructor () {
+            //this.name="Fred";
+            //this.food="cheerios";
+        
+        
+        move(){
+            console.log ("Dinosaurs named" + this.name + "move by chasing their prey");
+    
+        }
+        act(){
+            console.log ("Dinosaurs act very excited when watching Jurassic Park");
+    
+        }
+        roar(){
+            console.log ("Dinosaurs roar when they are hungry");
+    
+        }
+    } 
+    
+    const dino = new Dinosaur();
+
+    dino.move();
+    dino.act();
+    dino.roar();
 
 /*
-2. For the following Person class, modify the class methods, so the sub-class methods will successfully fire.  For the Teacher method, a set of methods have already been set up.  For the Student class, it will be up to you to determine how to set up the methods to fire in the Student sub-class.  :
+2. For the following Person class, modify the class methods, so the sub-class methods will successfully fire.  
+For the Teacher method, a set of methods have already been set up.  For the Student class, it will be up to you 
+to determine how to set up the methods to fire in the Student sub-class.  :
 */
 
 class Person {
